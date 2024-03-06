3ce7f93dba36277d9bc418cc217290f5dff56038
sukrutha3112 <sukrutha3112@gmail.com>
2024-03-06 08:54:52 +0530

    * project changes

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,7 +1,12 @@
-import express from 'express';
+import express from "express";
+import { login, logout, signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
-router.get('/login',(req,res)=>{
-    res.send("Login")
-})
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.post("/logout", logout);
+
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,35 @@
-// const express = require('express');
-// const dotenv = require('dotenv')
-
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
 dotenv.config();
-const app = express();
-const PORT = process.env.PORT;
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-// app.get('/api/auth/signup',(req,res)=>{
-//     res.send("hello")
-// })
-// app.get('/api/auth/login',(req,res)=>{
-//     res.send("hello")
-// })
-// app.get('/api/auth/logout',(req,res)=>{
-//     res.send("hello")
-// })
+
+app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
+app.use(cookieParser());
+
 app.use("/api/auth", authRoutes);
-app.listen(PORT, () => console.log(`Server at ${PORT}`));
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on port ${PORT}`);
+});
