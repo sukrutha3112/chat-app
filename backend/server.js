0479ad8b4591ef39375595e899b2e34c94df9e18
sukrutha3112 <sukrutha3112@gmail.com>
2024-03-07 10:30:00 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
+//middleware
 app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
 app.get("*", (req, res) => {
